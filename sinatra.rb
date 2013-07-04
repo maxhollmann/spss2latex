29f54c4c2e2fa5a9c96daedc55223aeba8ec06ac
Max Hollmann <mail@maxhollmann.de>
2013-07-05 01:20:18 +0200

    statsmix

--- sinatra.rb
+++ sinatra.rb
@@ -1,12 +1,16 @@
 require 'sinatra'
+require 'statsmix'
+
+require './config/statsmix'
 require './spss2latex'
 
-
 get '/' do
+  StatsMix.track('Page views on SPSS2LaTeX', 1, {:meta => {"page" => "index"}})
   erb :index
 end
 
 post '/' do
+  StatsMix.track('Page views on SPSS2LaTeX', 1, {:meta => {"page" => "convert"}})
   @latex = SPSS2Latex.convert(params[:tables])
   erb :convert
 end
