037b865f1008d0a944b9e46d3e4d19e4f7768141
Max Hollmann <mail@maxhollmann.de>
2013-09-19 16:28:35 +0200

    link to source code

--- sinatra.rb
+++ sinatra.rb
@@ -1,3 +1,4 @@
+require 'rubygems'
 require 'sinatra'
 require 'statsmix'
 
@@ -5,16 +6,16 @@ require './config/statsmix'
 require './spss2latex'
 
 get '/' do
-  StatsMix.track('Page views on SPSS2LaTeX', 1, {:meta => {"page" => "index"}})
+  StatsMix.track('Page views on SPSS2LaTeX', 1, {:meta => {"page" => "index"}}) if StatsMix.api_key
   erb :index
 end
 
 post '/' do
-  StatsMix.track('Page views on SPSS2LaTeX', 1, {:meta => {"page" => "convert"}})
+  StatsMix.track('Page views on SPSS2LaTeX', 1, {:meta => {"page" => "convert"}}) if StatsMix.api_key
   begin
     @latex = SPSS2Latex.convert(params[:tables])
   rescue Exception => e
-    StatsMix.track('Exceptions on SPSS2LaTeX', 1, {:meta => {"page" => "convert", "input" => params[:tables], "message" => e.message}})
+    StatsMix.track('Exceptions on SPSS2LaTeX', 1, {:meta => {"page" => "convert", "input" => params[:tables], "message" => e.message}}) if StatsMix.api_key
     @latex = "Sorry, I couldn't convert your table."
   end
   erb :convert
